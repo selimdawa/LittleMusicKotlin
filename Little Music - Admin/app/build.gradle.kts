94e775906c5d981a117d384350e1c026c5690305
selimdawa <selimdawa@gmail.com>
2026-04-10 02:34:51 +0300

    ADD minorApiLevel

--- Little Music - Admin/app/build.gradle.kts
+++ Little Music - Admin/app/build.gradle.kts
@@ -8,7 +8,9 @@ plugins {
 android {
     namespace = "com.flatcode.littlemusicadmin"
     compileSdk {
-        version = release(36)
+        version = release(36) {
+            minorApiLevel = 1
+        }
     }
 
     defaultConfig {
--- Little Music/app/build.gradle.kts
+++ Little Music/app/build.gradle.kts
@@ -8,7 +8,9 @@ plugins {
 android {
     namespace = "com.flatcode.littlemusic"
     compileSdk {
-        version = release(36)
+        version = release(36) {
+            minorApiLevel = 1
+        }
     }
 
     defaultConfig {
